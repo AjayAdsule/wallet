2cd0f8a1a381740968100aef9fbefaf57e9bc77c
Ajay A Adsule <ajayadsule5@gmail.com>
2024-11-21 15:47:49 +0530

    add seed phrase

--- wallet/src/Pages/CreateWallet/components/Seed.tsx
+++ wallet/src/Pages/CreateWallet/components/Seed.tsx
@@ -3,22 +3,36 @@ import { Input } from "@/components/ui/input";
 import { generateMnemonic } from "bip39";
 import { useState } from "react";
 
+/**
+ * Generates a BIP39 mnemonic seed phrase and displays it in a grid.
+ * When the phrase is generated, a "Next" button is displayed.
+ * The phrase is not displayed until the button is clicked.
+ * @returns A JSX.Element
+ */
 const Seed = () => {
   const [mnemonic, setMnemonic] = useState("");
 
+  /**
+   * Generates a BIP39 mnemonic seed phrase
+   */
   async function generateMnemonicPhrase() {
     const mn = await generateMnemonic();
     setMnemonic(mn);
   }
-  console.log(mnemonic.split(" ").length);
+
+  /**
+   * Whether the mnemonic phrase has been generated
+   */
+  const isMnemonicGenerated = mnemonic.length > 0;
+
   return (
     <div>
       <div
         className={`border h-[200px] w-[450px] p-2 mt-6  flex justify-center  items-center ${
-          mnemonic.length > 0 ? "" : "bg-foreground"
+          isMnemonicGenerated ? "" : "bg-foreground"
         }`}
       >
-        {mnemonic.length > 0 ? (
+        {isMnemonicGenerated ? (
           <div className="grid grid-cols-4 gap-2">
             {mnemonic.split(" ")?.map((item, index) => (
               <Input value={item} key={index} readOnly />
@@ -32,7 +46,7 @@ const Seed = () => {
       </div>
       <div className="flex mt-4  justify-center">
         <Button className="" onClick={generateMnemonicPhrase}>
-          {mnemonic.length > 0 ? "Next" : " Reveal Secret Recovery Phrase"}
+          {isMnemonicGenerated ? "Next" : " Reveal Secret Recovery Phrase"}
         </Button>
       </div>
     </div>
