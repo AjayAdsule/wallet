b416ab567ff03a49b304b0d11a02921d418a2d0f
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2024-11-23 15:40:31 +0530

    use context instead of local storage

--- wallet/src/Pages/HomePage/component/Accounts.tsx
+++ wallet/src/Pages/HomePage/component/Accounts.tsx
@@ -1,18 +1,17 @@
-import { SecureStorage } from "@/utils/secureStorage ";
+import { useWeb3Context } from "@/context/WebContext";
 import React from "react";
 
 const Accounts: React.FC = () => {
-  const storageAddress = localStorage.getItem("vault_address");
-  const address = SecureStorage.decrypt(JSON.parse(storageAddress as string));
+  const { web3State } = useWeb3Context();
 
-  const formatAddress = (address: string) => {
-    return `${address.slice(0, 6)}...${address.slice(-6)}`;
+  const formatAddress = (address: string | null) => {
+    return `${address?.slice(0, 6)}...${address?.slice(-6)}`;
   };
-  const firstAddress = formatAddress(JSON.parse(address)[0]);
+
   return (
     <div className=" flex flex-col  items-center justify-center gap-y-1">
       <div>Account 1</div>
-      <div className="text-sm">{firstAddress}</div>
+      <div className="text-sm">{formatAddress(web3State.account) ?? 0}</div>
     </div>
   );
 };
